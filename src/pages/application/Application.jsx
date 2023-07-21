7d4d672517c992ca91e1c8c5af236ddbf40421b4
Akash Rai <akash03rai@gmail.com>
2023-07-21 20:20:26 +0530

    added difficulty

--- src/pages/application/Application.jsx
+++ src/pages/application/Application.jsx
@@ -36,19 +36,20 @@ export default function Application() {
 
   // loading
   const [loading, setLoading] = useState(false);
-
   const [timeLimit, setTimeLimit] = useState(15);
 
   // game finished
   const [gameFinished, setGameFinished] = useState(false); //state for game finished
 
-  const getWords = async () => {
-    //data fetching
+  //difficlty
+  const [difficulty, setDifficulty] = useState(1);
 
+  const getWords = async () => {
     setLoading(true); //data is loading
     let x = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
     const tempUrl = "http://metaphorpsum.com/paragraphs/1/" + x.toString();
-    const { data } = await axios.get(tempUrl);
+    var { data } = await axios.get(tempUrl);
+    if(difficulty == 1) data = data.toLowerCase();
     const fetchParagraph = data.split(" ");
     setWords(fetchParagraph);
     setLoading(false);
@@ -86,7 +87,7 @@ export default function Application() {
         const currentDate = month + "/" + date + "/" + year
         const time = today.getHours()
           + ':' + today.getMinutes()
-          + ":" + today.getSeconds()
+          + ":" + today.getSeconds();
 
         const fixedResults = {
           WPM: WPM.toFixed(2),
@@ -124,8 +125,11 @@ export default function Application() {
     if (timeElapsed >= timeLimit) checkInput(" ");
   }, [timeElapsed]);
 
+  useEffect(() => {
+    getWords();
+  },[difficulty])
+
   const restartGame = () => {
-    // game reset
     setGameFinished(false);
     getWords();
     setUserInput("");
@@ -134,7 +138,7 @@ export default function Application() {
     setCorrectWords([]);
     setIncorrectWords([]);
     setStartCounting(false);
-    settimeLimit(15);
+    setTimeLimit(15);
     setProgress(0);
   };
 
@@ -142,8 +146,10 @@ export default function Application() {
     <>
       <div
         className="flex flex-col justify-start md:justify-center items-center"
-        style={{ marginTop: "10rem" }}
+        style={{ marginTop: "8rem" }}
       >
+
+
         {loading ? ( // when it start loading
           <div className="flex justify-center">
             <div className="spinner">
@@ -173,6 +179,9 @@ export default function Application() {
                 setTimeLimit={setTimeLimit}
                 WPM={WPM}
                 setWPM={setWPM}
+                difficulty={difficulty}
+                setDifficulty={setDifficulty}
+                getWords = {getWords}
               />
               <div
                 className="w-1/2 p-8 rounded-lg renderBlur"
@@ -228,6 +237,9 @@ export default function Application() {
                 setTimeLimit={setTimeLimit}
                 WPM={WPM}
                 setWPM={setWPM}
+                difficulty={difficulty}
+                setDifficulty={setDifficulty}
+                getWords = {getWords}
               />
 
               <div
